004054ac67716d97fa23974388bc573f60dc5fac
Mathis <mathis.morales@ou.edu>
2024-06-28 11:20:58 -0500

    removed deprecated bash file

--- Tracking/GT_launch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-python workfile.py --gt_track --detection_method GT --data_root ./data/nuScenes --cat_detection_root ./data/cat_detection/
-
-python workfile.py --gt_track --detection_method GT --data_root ./data/nuScenes --cat_detection_root ./data/cat_detection/ --concat
-
-
-python evaluate.py --result_path output/track_output_GT/track_results_nusc.json --output_dir output/track_output_GT/ \
-                    --eval_set val --dataroot ./data/nuScenes 
